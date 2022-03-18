11fa5aba8ac6ef2453f713fc1057c7fd2de0ef6a
spikeboss <cajlonbatista@puzzl.com.br>
2022-03-18 19:52:20 -0300

    build(monorepo): remove plugins stencil from package.json

--- storybook/.storybook/main.js
+++ storybook/.storybook/main.js
@@ -1,11 +1,11 @@
 module.exports = {
   stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
+    'storybook-dark-mode',
+    'storybook-addon-themes',
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    'storybook-addon-themes',
-    'storybook-dark-mode',
-    '@etchteam/storybook-addon-status',
     '@storybook/addon-storysource',
+    '@etchteam/storybook-addon-status',
   ],
 };
